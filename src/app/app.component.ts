dd9238f5687ff32da736869e1d8951abceca2533
Igor Santos <igornzds@gmail.com>
2021-01-29 22:47:00 -0300

    Iniciar a adição de eventos com menuMobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  menuOpened = false;
+
+  setMenuState(state: boolean) {
+    this.menuOpened = state;
+  }
+
+  closeMenu() {
+    this.menuOpened = false;
+  }
+
+  listTitles = [
+    {
+    trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 1',
+    relevance: 98,
+    age: 0,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },
+    {
+    trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 2',
+    relevance: 80,
+    age: 0,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },
+    {
+    trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 3',
+    relevance: 55,
+    age: 18,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },
+    {
+    trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 4',
+    relevance: 55,
+    age: 16,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },
+];
 }
