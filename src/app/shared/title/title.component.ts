7c68e7deef8865ea8bc7bb8e6968fc19b520b4bf
Igor Santos <igornzds@gmail.com>
2021-01-30 09:41:41 -0300

    Finalizar a adição de eventos: togglePlayFilmes

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -8,10 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TitleComponent implements OnInit {
 
   @Input() title: any;
+  @ViewChild('trailer', { static: true }) trailer: ElementRef<HTMLVideoElement>;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  playVideo() {
+    this.trailer.nativeElement.play();
+  }
+
+  stopVideo() {
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.pause();
+  }
+
+  getMuted(): boolean {
+    return this.trailer.nativeElement.muted;
+  }
+
+  toggleSound() {
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted; 
   }
 
 }
